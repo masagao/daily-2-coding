8eed2980e123ecb1e6bc28b7a6a02b6809d0c31e
masagao <masanao@gaogao.asia>
2021-04-11 18:35:44 +0900

    add postorder rb

--- min_ruby/tree.rb
+++ min_ruby/tree.rb
@@ -1,13 +1,23 @@
 def preorder(tree)
-    if tree[0].start_with?("葉")
-        p(tree[0])
-    end
+    # if tree[0].start_with?("葉")
+    #     p(tree[0])
+    # end
+    p(tree[0])
     if tree[0].start_with?("節")
         preorder(tree[1])
         preorder(tree[2])
     end
 end
 
+def postorder(tree)
+    if tree[0].start_with?("節")
+        postorder(tree[1])
+        postorder(tree[2])
+    end
+    p(tree[0])
+end
+
 node = ["節1", ["節2", ["葉A"], ["葉B"]], ["節3", ["葉C"], ["葉D"]]]
 
-preorder(node);
+# preorder(node);
+postorder(node);
